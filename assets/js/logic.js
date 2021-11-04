f822f0d618aeebdc00d06eb058ff843f07a4c681
gjspencer22 <gjspencer22@gmail.com>
2021-11-03 23:00:56 -0600

    started on random project button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,1 @@
-card.addEventListener('mouseleave', function (e) {
-
-  this.style.setProperty('--mouseX', 0);
-  this.style.setProperty('--mouseY', 0);
-
-});
+document.getElementById('randomProjectButton')
