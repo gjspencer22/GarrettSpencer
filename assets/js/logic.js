3cc3f5451ccb26c7f232d2aa129be3b3473c11a5
gjspencer22 <gjspencer22@gmail.com>
2021-11-04 22:53:37 -0600

    added links for random project button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,7 @@
 document.getElementById('randomProjectButton').addEventListener('click', randomProject);
 
 function randomProject(){
-   const projects= ["https://www.google.com", "https://www.youtube.com"]
+   const projects= ["https://desolate-harbor-29412.herokuapp.com/", "https://drive.google.com/file/d/1o4c_Acu3aNr-qFY1ugLgjMaQYFfcStlW/view"]
    let randomProjects= projects[Math.floor(Math.random()* projects.length)];
   console.log('oy')
   window.open(randomProjects);
