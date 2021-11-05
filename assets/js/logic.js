e74a374eb80ce7345b50e73abc09d519dc5cade9
gjspencer22 <gjspencer22@gmail.com>
2021-11-04 22:45:26 -0600

    added logic for random project button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,8 @@
-document.getElementById('randomProjectButton')
+document.getElementById('randomProjectButton').addEventListener('click', randomProject);
+
+function randomProject(){
+   const projects= ["https://www.google.com", "https://www.youtube.com"]
+   let randomProjects= projects[Math.floor(Math.random()* projects.length)];
+  console.log('oy')
+  window.open(randomProjects);
+}
